d6eb10bf1c1ebc4c3da098cd860ab7ca3871d0d1
Yewon-Song <supernova2000@naver.com>
2026-01-30 03:26:06 +0900

    Update dataset paths and source model references in configuration files; add new kitti2nusc.yaml for adaptation.

--- eval_only.sh
+++ eval_only.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Activate conda environment
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate d3ctta
 
 # Run eval_only for no adaptation baseline for SemanticSTF
-CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file /home/yewon/project/tta/D3CTTA/configs/adaptation/test-stf/nusc2stf.yaml
-CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file /home/yewon/project/tta/D3CTTA/configs/adaptation/test-stf/sk2stf.yaml
-CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file /home/yewon/project/tta/D3CTTA/configs/adaptation/test-stf/synth2stf.yaml
+CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file configs/adaptation/test-stf/nusc2stf.yaml
+CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file configs/adaptation/test-stf/sk2stf.yaml
+CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file configs/adaptation/test-stf/synth2stf.yaml
 
-# Run eval_only for no adaptation baseline for SemanticKITTI-C
-CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file /home/yewon/project/tta/D3CTTA/configs/adaptation/test-kitti/nusc2kitti.yaml
-CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file /home/yewon/project/tta/D3CTTA/configs/adaptation/test-kitti/sk2kitti.yaml
-CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file /home/yewon/project/tta/D3CTTA/configs/adaptation/test-kitti/synth2kitti.yaml
+# # Run eval_only for no adaptation baseline for SemanticKITTI-C
+# CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file configs/adaptation/test-kitti-c/nusc2kitti.yaml
+# CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file configs/adaptation/test-kitti-c/sk2kitti.yaml
+# CUDA_VISIBLE_DEVICES=0 python eval_only.py --config_file configs/adaptation/test-kitti-c/synth2kitti.yaml
 
-"
+# echo "All evaluation processes are completed."
